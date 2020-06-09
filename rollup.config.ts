29109e16b733522a7eac93b92bd6843d698bac13
Hosanna <ohosanna@gmail.com>
2020-06-09 11:20:00 +0800

    组件升级&配置优化

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,9 +1,10 @@
-import resolve from 'rollup-plugin-node-resolve'
-import commonjs from 'rollup-plugin-commonjs'
+import resolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
 import sourceMaps from 'rollup-plugin-sourcemaps'
 import camelCase from 'lodash.camelcase'
 import typescript from 'rollup-plugin-typescript2'
-import json from 'rollup-plugin-json'
+import json from '@rollup/plugin-json'
+import { terser } from "rollup-plugin-terser"
 
 const pkg = require('./package.json')
 
@@ -12,8 +13,8 @@ const libraryName = '--libraryname--'
 export default {
   input: `src/${libraryName}.ts`,
   output: [
-    { file: pkg.main, name: camelCase(libraryName), format: 'umd', sourcemap: true },
-    { file: pkg.module, format: 'es', sourcemap: true },
+    { file: pkg.browser, name: camelCase(libraryName), format: 'umd', sourcemap: true, plugins: [terser()] },
+    { file: pkg.module, format: 'esm', sourcemap: true },
   ],
   // Indicate here external modules you don't wanna include in your bundle (i.e.: 'lodash')
   external: [],
